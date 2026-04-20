fb63c3a33802c67afb52c67e8c91af7287257951
Md. Saikat Islam <saikatislam709@gmail.com>
2026-04-20 18:06:37 +0600

    project halfway

---
+++ components/installation.tsx
@@ -0,0 +1,16 @@
+export default function Installation() {
+    return (
+        <section id="installation" className="w-full px-5 overflow-hidden flex flex-col justify-start py-6 md:py-8 lg:py-14">
+            <div className="self-stretch py-6 md:py-8 lg:py-14 flex flex-col justify-center items-center gap-2">
+                <div className="flex flex-col justify-start items-center gap-4">
+                    <h2 className="text-center text-foreground text-3xl md:text-4xl lg:text-[40px] font-semibold leading-tight md:leading-tight lg:leading-[40px]">
+                        Installation
+                    </h2>
+                    <p className="self-stretch text-center text-muted-foreground text-sm md:text-sm lg:text-base font-medium leading-[18.20px] md:leading-relaxed lg:leading-relaxed">
+                        Everything you need to know about BetterCF and how it can transform your development workflow
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
